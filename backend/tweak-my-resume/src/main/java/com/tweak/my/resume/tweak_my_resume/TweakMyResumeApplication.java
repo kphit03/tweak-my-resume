6b235f1a1c69a3ed6adcf3164b00c21498f98a6c
kphitsan <kevinphitsanu22@gmail.com>
2025-08-17 06:28:07 -0400

    First commit

---
+++ backend/tweak-my-resume/src/main/java/com/tweak/my/resume/tweak_my_resume/TweakMyResumeApplication.java
@@ -0,0 +1,12 @@
+package com.tweak.my.resume.tweak_my_resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweakMyResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweakMyResumeApplication.class, args);
+	}
+}
---
+++ backend/tweak-my-resume/src/test/java/com/tweak/my/resume/tweak_my_resume/TweakMyResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweak.my.resume.tweak_my_resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweakMyResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
