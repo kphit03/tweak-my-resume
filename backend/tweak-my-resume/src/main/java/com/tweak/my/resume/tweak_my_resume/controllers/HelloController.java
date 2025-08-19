32f996968b0111603cd190ad4c391f1f5c64dfa0
kphitsan <kevinphitsanu22@gmail.com>
2025-08-19 03:29:34 -0400

    Setup basic OAuth2.

--- backend/tweak-my-resume/src/main/java/com/tweak/my/resume/tweak_my_resume/controllers/HelloController.java
+++ backend/tweak-my-resume/src/main/java/com/tweak/my/resume/tweak_my_resume/controllers/HelloController.java
@@ -4,15 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class HelloController {
 
     @GetMapping("/")
-    public String home() {
-        return "Hello Home";
-    }
-
-    @GetMapping("/secured")
-    public String secured() {
-        return "Hello Secured";
+    public String sayHello(){
+        return "Hello World";
     }
 }
