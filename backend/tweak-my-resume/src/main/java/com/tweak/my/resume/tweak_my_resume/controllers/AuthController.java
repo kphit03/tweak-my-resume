e33120179433b08e4494190ca4106207e74d4654
kphitsan <kevinphitsanu22@gmail.com>
2025-08-23 15:30:19 -0400

    Revised OAuth2 flow through application.
    Implemented test request with axios to prep for
    backend and frontend integration.

---
+++ backend/tweak-my-resume/src/main/java/com/tweak/my/resume/tweak_my_resume/controllers/AuthController.java
@@ -0,0 +1,4 @@
+package com.tweak.my.resume.tweak_my_resume.controllers;
+
+public class AuthController {
+}
--- backend/tweak-my-resume/src/main/java/com/tweak/my/resume/tweak_my_resume/security/SecurityConfig.java
+++ backend/tweak-my-resume/src/main/java/com/tweak/my/resume/tweak_my_resume/security/SecurityConfig.java
@@ -1,26 +1,4 @@
 package com.tweak.my.resume.tweak_my_resume.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers( "/" , "/api/auth/me").permitAll()
-                        .requestMatchers( "/hello").authenticated()
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(Customizer.withDefaults());
-
-        return http.build();
-    }
 }
